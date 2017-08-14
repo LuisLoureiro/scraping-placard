22fbc77a12d29423e49811075e0db1948ee42348
Loureiro <LuisLoureiro@users.noreply.github.com>
2017-08-14 15:56:59 +0100

    Event object should contain the information about the sport, country and competition from where it belongs to.

--- models/event.js
+++ models/event.js
@@ -1,10 +1,13 @@
 class Event {
-  constructor (code, home, away, date, bets) {
+  constructor (code, home, away, date, bets, sport, country, competition) {
     this.code = code
     this.home = home
     this.away = away
     this.date = date
     this.bets = bets || []
+    this.sport = sport
+    this.country = country
+    this.competition = competition
   }
 
   getName () {
