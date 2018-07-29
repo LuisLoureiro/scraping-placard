ef5dd4ac8773a36e65f720798c52f598a85020ff
Luís Loureiro <LuisLoureiro@users.noreply.github.com>
2018-07-29 22:47:27 +0100

    Fix problem with exact result bet type: - this bet type can be crawled differently depending on the sport.

--- fetch/scrapers/betTypes/exactResult.js
+++ fetch/scrapers/betTypes/exactResult.js
@@ -2,14 +2,14 @@ const OptionsAvailableInTable = require('./optionsAvailableInTable')
 
 module.exports = class ExactResult extends OptionsAvailableInTable {
   getCodeElem () {
-    return this.$('.marketIndex_correct_score')
+    return this.$('.marketIndex_correct_score, .marketIndex')
   }
 
   getDateElem () {
-    return this.$('.date_correct_score')
+    return this.$('.date_correct_score, .date')
   }
 
   getTimeElem () {
-    return this.$('.time_correct_score')
+    return this.$('.time_correct_score, .time')
   }
 }
